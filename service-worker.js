e95e4cf11ac02bfe75a2fe9df10a1caf5ae9ff87
rielOosh <riel.oosh@gmail.com>
2025-09-02 17:05:25 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // A descriptive cache name helps in managing updates.
 // Incrementing the version (e.g., to 'v10') will trigger the 'activate' event for cleanup.
-const CACHE_NAME = 'grappa-guest-guide-v10';
+const CACHE_NAME = 'grappa-guest-guide-v0';
 
 // A comprehensive list of assets to cache for a full offline experience.
 const URLS_TO_CACHE = [
