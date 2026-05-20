752aafe810ee5ab8945131c522eab01b53c359d2
rielOosh <riel.oosh@gmail.com>
2026-05-20 04:23:12 +0300

    fix: suppress dragging during map rotation and align compass heading to map bearing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'grappa-guest-guide-v27';
+const CACHE_NAME = 'grappa-guest-guide-v28';
 
 // A comprehensive list of assets to cache for a full offline experience.
 const URLS_TO_CACHE = [
