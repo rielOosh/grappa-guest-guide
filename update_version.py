710fc416c69c1d2e2c49ef707495aee4a54cbe44
rielOosh <riel.oosh@gmail.com>
2026-07-15 22:55:12 +0300

    Delete update_version.py

--- update_version.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('index.html', 'r') as f:
-    content = f.read()
-
-content = re.sub(r'swiming_pool_instructions\.png\?v=\d+', 'swiming_pool_instructions.png?v=7', content)
-
-with open('index.html', 'w') as f:
-    f.write(content)
